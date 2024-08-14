ce20a9ac2e5bf1799ba18bf455c32bd8baa3beb9
chromaticpipe <127907268+chromaticpipe@users.noreply.github.com>
2024-08-14 16:54:50 -0500

    i dont know what im doing anymore

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -19,4 +19,25 @@ app.ports.addFile.subscribe((message) => {
   });
 
   input.click();
-});
+}); 
+
+
+var Module = Module || {};
+
+var InitializeFS = () => {
+  FS.mkdirTree('/addons');
+  FS.symlink('/home/web_user/.srb2', '/addons/.srb2');
+  FS.mount(IDBFS, {}, '/home/web_user');
+  return (new Promise((resolve, reject) => {
+    FS.syncfs(true, (err) => {
+      console.log("SyncFS done");
+      console.log(err);
+      resolve();
+    });
+  }));
+};  
+
+Module.onRuntimeInitialized = function() {  
+  InitializeFS()
+}
+
